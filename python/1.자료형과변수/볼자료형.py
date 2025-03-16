964eea9588fe43169e80022fb5c9608dc4c2d266
kimgunkrok123 <kimgunkrok@gmail.com>
2025-03-16 22:15:33 +0900

    1.자료형과 변수

---
+++ python/1.자료형과변수/볼자료형.py
@@ -0,0 +1,12 @@
+#비교
+print(1>12)
+print(1<12)
+#볼자료형출력
+print(False)
+print(True)
+#not연산자이용
+print(not False)
+print(not True)
+#수식을 부정
+not 1>12
+print(not (1>12))
---
+++ python/1.자료형과변수/형변환.py
@@ -0,0 +1,11 @@
+a = 1
+b = 31
+c = 2.1
+
+#숫자 => 문자열
+print(str(b))
+#소수 => 정수
+print(int(c))
+#정수 => 소수
+print(float(a))
+
